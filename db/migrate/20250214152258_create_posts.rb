bae4e61c335828f3c373aff0ff06493a784521e3
Javier Garcia <jgarcia@lwolf.com>
2025-02-14 13:11:15 -0500

    create model and migration for it

---
+++ db/migrate/20250214152258_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[7.2]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :category
+      t.json :tags
+
+      t.timestamps
+    end
+  end
+end
